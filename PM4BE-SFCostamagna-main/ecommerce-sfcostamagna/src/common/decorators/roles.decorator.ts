bf0f081771dd9e0a464abadb735ec10ceb8d1e08
SCostamagna <stefacostamagna@gmail.com>
2026-03-17 07:26:40 -0300

    Delete PM4BE-SFCostamagna-main directory

--- PM4BE-SFCostamagna-main/ecommerce-sfcostamagna/src/common/decorators/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '../enums/roles.enum';
-
-export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
-
-//Crea una función que recibe una lista de roles y los guarda bajo una "llave" llamada 'roles'en un []
-// Si hacés @Roles(Role.Admin), el decorador dice: guardame en esta ruta el valor ['admin'] bajo la llave 'roles'
